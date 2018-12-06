fb0192069c733949ac803d7480c676e2715a177f
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-12-06 12:11:23 +1100

    Add restart button in nav bar, change to pills for nav instead of tabs.

--- dashboard/gateway/routes/dashboard.js
+++ dashboard/gateway/routes/dashboard.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 app.locals.console_url = process.env.CONSOLE_URL;
+app.locals.restart_url = process.env.RESTART_URL;
 
 app.set('views', '/opt/workshop/gateway/views');
 app.set('view engine', 'pug');
