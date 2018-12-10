6ea27af622d86a51e27983edeae1d3a529858c83
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-12-10 11:06:01 +1100

    Cleanup up config and allow overriding of config when alternate content supplied.

--- dashboard/raneto/config.js
+++ dashboard/raneto/config.js
@@ -1,109 +1,138 @@
-
 'use strict';
 
-// Modules
 var path = require('path');
 var fs = require('fs');
+var uuid = require('uuid');
 
 var config = {
 
-    // Your site title (format: page_title - site_title)
+    // Site title. Appears in page banner.
+
     site_title: 'Workshop',
 
-    // The base URL of your site (can use %base_url% in Markdown files)
+    // The base URL of the site. Don't override this if hosting terminal
+    // in conjunction with JupyterHub instance manager.
+
     base_url: process.env.URI_ROOT_PATH,
 
-    // Used for the "Get in touch" page footer link
+    // Contact email address, used in the "Get in touch" page footer link.
+
     // support_email: '',
 
-    // Footer Text / Copyright
-    // copyright: 'Copyright &copy; ' + new Date().getFullYear() + ' - <a href="http://raneto.com">Powered by Raneto</a>',
+    // Copyright designation for any content if necessart.
 
-    // Excerpt length (used in search)
+    // copyright: 'Copyright &copy; ' + new Date().getFullYear() + ' Author',
+
+    // Amount of content to be shown from a page in search results.
+
     excerpt_length: 400,
 
-    // The meta value by which to sort pages (value should be an integer)
-    // If this option is blank pages will be sorted alphabetically
+    // The meta value by which to sort pages (value should be an integer).
+    // If this option is blank pages will be sorted alphabetically.
+
     page_sort_meta: 'sort',
 
-    // Should categories be sorted numerically (true) or alphabetically (false)
-    // If true category folders need to contain a "sort" file with an integer value
+    // Should categories be sorted numerically (true) or alphabetically
+    // (false). If true category folders need to contain a "sort" file
+    // with an integer value
+
     category_sort: true,
 
-    // Controls behavior of home page if meta ShowOnHome is not present. If set to true
-    // all categories or files that do not specify ShowOnHome meta property will be shown
+    // Controls behavior of home page if meta ShowOnHome is not present.
+    // If set to true all categories or files that do not specify
+    // ShowOnHome meta property will be shown
+
     show_on_home_default: true,
 
-    // Which Theme to Use?
-    //theme_dir  : path.join(__dirname, 'node_modules', 'raneto', 'themes'),
+    // Which Theme to Use? Do not override this unless you are providing
+    // a complete copy of all the templates.
+
     theme_dir  : path.join(__dirname, 'themes'),
     theme_name : 'default',
 
-    // Specify the path of your content folder where all your '.md' files are located
-    // Fix: Needs trailing slash for now!
-    // Fix: Cannot be an absolute path
+    // Specify the path of your content folder where all your '.md'
+    // files are located. Do not override this, it will be adjusted
+    // down below to look in other vali places you can supply content.
+
     content_dir : path.join(__dirname, 'content'),
 
-    // Where is the public directory or document root?
-    public_dir  : path.join(__dirname, 'node_modules', 'raneto', 'themes', 'default', 'public'),
+    // The public directory or document root for static files. Do not
+    // override this, the routes are set up separately to look in other
+    // directories corresponding to where you can provide content.
 
-    // The base URL of your images folder,
-    // Relative to config.public_dir
-    // (can use %image_url% in Markdown files)
+    public_dir  : path.join(__dirname, 'node_modules', 'raneto', 'themes',
+        'default', 'public'),
+
+    // The base URL for the images folder. Don't override this, extra
+    // routes are setup so you can provide images in your content
+    // directories rather than having to place them in a separate images
+    // directory.
+
     image_url: process.env.URI_ROOT_PATH + '/images',
 
-    // Add your analytics tracking code (including script tags)
-    //analytics: '',
+    // Add your analytics tracking code (including script tags).
 
-    // Set to true to enable the web editor
-    //allow_editing : true,
+    // analytics: '',
+
+    // Controls whether content can be edited through the web interface.
+
     allow_editing : false,
 
-    // Set to true to enable HTTP Basic Authentication
-    //authentication : true,
+    // Controls whether authentication is required. This enables HTTP
+    // Basic authentication.
+
     authentication : false,
 
-    // If editing is enabled, set this to true to only authenticate for editing, not for viewing
+    // If editing is enabled, controls whether authentication is
+    // required to be able to edit content through the web interface.
+
     authentication_for_edit: true,
 
-    // If authentication is enabled, set this to true to enable authentication for reading too
+    // If authentication is enabled, controls whether authentication
+    // is also required simply to read content.
+
     authentication_for_read: false,
 
+    // Controls whether Google OAuth authentication rather than HTTP
+    // Basic authentication should be used. Don't override this.
+
     googleoauth: false,
-    //oauth2 : {
-    //   client_id: 'GOOGLE_CLIENT_ID',
-    //   client_secret: 'GOOGLE_CLIENT_SECRET',
-    //   callback: 'http://localhost:3000/auth/google/callback',
-    //   hostedDomain: 'google.com'
-    //},
-    secret: 'someCoolSecretRightHere',
 
-    credentials: [
-         {
-           username : 'admin',
-           password : 'password'
-         },
-         {
-           username : 'admin2',
-           password : 'password'
-         }
-     ],
+    // Secret for use in generating cookies or other secrets.
 
-    //locale: 'en',
+    secret: uuid.v4(),
 
-    // Sets the format for datetime's
+    // List of username, password pairs if authentication is enabled
+    // and HTTP Basic authentication is used.
+
+    credentials: [],
+
+    // Locate for translations.
+
+    locale: 'en',
+
+    // Format for date/time values.
+
     datetime_format: 'Do MMM YYYY',
 
-    // Set to true to render suitable layout for RTL languages
+    // Controls whether the layout is suitable for rendering RTL languages.
+
     rtl_layout: false,
 
-    // Edit Home Page title, description, etc.
+    // Controls page title and description in page meta information.
+
     home_meta: {
         title: 'Workshop',
         description: 'Workshop'
     },
 
+    // Controls whether table of contents generation enabled for pages.
+
     table_of_contents: false,
+
+    // List of variables available for interpolation in content. Where a
+    // user supplied config.js exists, entries from it will be appended
+    // to these.
 
     variables: [
       {
@@ -120,15 +149,43 @@ var config = {
       }
     ],
 
+    // URL where users should be redirected to restart the workshop.
+
     restart_url: process.env.RESTART_URL
 };
 
+var config_file;
+
+// Check for alternate locations for content.
+
 if (fs.existsSync('/opt/app-root/src/raneto/content')) {
     config.content_dir = '/opt/app-root/src/raneto/content';
+    config_file = '/opt/app-root/src/raneto/config.js';
 }
 else if (fs.existsSync('/opt/app-root/raneto/content')) {
     config.content_dir = '/opt/app-root/raneto/content';
+    config_file = '/opt/app-root/raneto/config.js';
 }
 
-// Exports
+// If a config.js is supplied with alternate content, merge it with the
+// configuration above.
+
+if (config_file && fs.existsSync(config_file)) {
+    var config_overrides = require(config_file);
+    for (var key1 in config_overrides) {
+        var value1 = config_overrides[key1];
+        if (value1.constructor == Array) {
+            config[key1].concat(value);
+        }
+        else if (value1.constructor == Object) {
+            for (var key2 in value1) {
+                config[key1][key2] = value1[key2];
+            }
+        }
+        else {
+            config[key1] = value1;
+        }
+    }
+}
+
 module.exports = config;
