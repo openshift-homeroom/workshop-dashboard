c0ca880bda09d1b28276dd3db7526e0196c62393
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-01-10 12:30:35 +1100

    Install reveal.js and require user to only supply index.html.

--- dashboard/gateway/routes/slides.js
+++ dashboard/gateway/routes/slides.js
@@ -3,11 +3,13 @@ var fs = require('fs');
 
 var app = express();
 
-if (fs.existsSync('/opt/app-root/src/workshop/slides')) {
+if (fs.existsSync('/opt/app-root/src/workshop/slides/index.html')) {
     app.use(express.static('/opt/app-root/src/workshop/slides'));
+    app.use(express.static('/opt/workshop/reveal.js'));
 }
-else if (fs.existsSync('/opt/app-root/workshop/slides')) {
+else if (fs.existsSync('/opt/app-root/workshop/slides/index.html')) {
     app.use(express.static('/opt/app-root/workshop/slides'));
+    app.use(express.static('/opt/workshop/reveal.js'));
 }
 
 module.exports = app
