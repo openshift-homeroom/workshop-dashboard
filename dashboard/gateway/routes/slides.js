1278361b8339263d3d46163a634877efaaf564af
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-01-17 10:30:26 +1100

    Allow environment variables to override workshop, content, slides and config file location.

--- dashboard/gateway/routes/slides.js
+++ dashboard/gateway/routes/slides.js
@@ -3,12 +3,27 @@ var fs = require('fs');
 
 var app = express();
 
-if (fs.existsSync('/opt/app-root/src/workshop/slides/index.html')) {
-    app.use(express.static('/opt/app-root/src/workshop/slides'));
-    app.use(express.static('/opt/workshop/reveal.js'));
+var workshop_dir = process.env.WORKSHOP_DIR || '/opt/app-root/src/workshop';
+
+var slides_dir = process.env.SLIDES_DIR;
+
+if (slides_dir) {
+    if (!fs.existsSync(slides_dir + '/index.html')) {
+        slides_dir = undefined;
+    }
 }
-else if (fs.existsSync('/opt/app-root/workshop/slides/index.html')) {
-    app.use(express.static('/opt/app-root/workshop/slides'));
+
+if (!slides_dir) {
+    if (fs.existsSync(workshop_dir + 'slides/index.html')) {
+        slides_dir = workshop_dir + '/slides';
+    }
+    else if (fs.existsSync('/opt/app-root/workshop/slides/index.html')) {
+        slides_dir = '/opt/app-root/workshop/slides';
+    }
+}
+
+if (slides_dir) {
+    app.use(express.static(slides_dir));
     app.use(express.static('/opt/workshop/reveal.js'));
 }
 
