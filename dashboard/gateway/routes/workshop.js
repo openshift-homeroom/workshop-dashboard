8fcd5bd93954b7713c98bd83c3ec1ecfc6c4c67c
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-02-24 22:24:27 +1100

    Add support for using workshopper for workshop content.

--- dashboard/gateway/routes/workshop.js
+++ dashboard/gateway/routes/workshop.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var proxy = require('http-proxy-middleware');
 
+workshops_urls = process.env.WORKSHOPS_URLS;
+
 module.exports = function(app, prefix) {
     var router = express.Router();
 
-    router.use(proxy(prefix, {
-        target: 'http://127.0.0.1:10082',
-        pathRewrite: {
-            ['^' + prefix]: ''
-        },
-    }));
+    if (workshops_urls) {
+        // Workshopper.
+        router.use(proxy(prefix, {
+            target: 'http://127.0.0.1:10082',
+            ws: true
+        }));
+    }
+    else {
+        // Raneto.
+        router.use(proxy(prefix, {
+            target: 'http://127.0.0.1:10082',
+            pathRewrite: {
+                ['^' + prefix]: ''
+            },
+        }));
+    }
 
     return router;
 }
