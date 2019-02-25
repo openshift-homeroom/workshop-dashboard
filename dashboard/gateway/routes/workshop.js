0838c64021b662c50ac4c933cd3e5b0046a60d71
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-02-25 11:36:43 +1100

    Add mechanism to wait for workshop to be ready when dashboard first opened.

--- dashboard/gateway/routes/workshop.js
+++ dashboard/gateway/routes/workshop.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var proxy = require('http-proxy-middleware');
+var axios = require('axios');
+var axios_retry = require('axios-retry');
 
 workshops_urls = process.env.WORKSHOPS_URLS;
 
@@ -8,6 +10,25 @@ module.exports = function(app, prefix) {
 
     if (workshops_urls) {
         // Workshopper.
+
+	router.get('/.redirect-when-workshop-is-ready', function (req, res) {
+	    var client = axios.create({ baseURL: 'http://127.0.0.1:10082' });
+
+            var options = {
+                retries: 3,
+                retryDelay: (retryCount) => {
+                    return retryCount * 500;
+                }
+            };
+
+	    axios_retry(client, options);
+
+	    client.get(req.baseUrl + '/')
+		.then(result => {
+		    res.redirect(req.baseUrl + '/');
+	        });
+	})
+
         router.use(proxy(prefix, {
             target: 'http://127.0.0.1:10082',
             ws: true
@@ -15,6 +36,25 @@ module.exports = function(app, prefix) {
     }
     else {
         // Raneto.
+
+	router.get('/.redirect-when-workshop-is-ready', function (req, res) {
+	    var client = axios.create({ baseURL: 'http://127.0.0.1:10082' });
+
+            var options = {
+                retries: 3,
+                retryDelay: (retryCount) => {
+                    return retryCount * 500;
+                }
+            };
+
+	    axios_retry(client, options);
+
+	    client.get('/')
+		.then(result => {
+		    res.redirect(req.baseUrl + '/');
+	        });
+	})
+
         router.use(proxy(prefix, {
             target: 'http://127.0.0.1:10082',
             pathRewrite: {
