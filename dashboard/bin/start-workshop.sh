2cfc67e2f9155e79364298ce7c9db8918c3f044d
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-04-04 19:45:45 +1100

    Delete redundant code.

--- dashboard/bin/start-workshop.sh
+++ dashboard/bin/start-workshop.sh
@@ -8,13 +8,11 @@ fi
 
 if [ -f /opt/app-root/workshop/_workshop.yml ]; then
     export WORKSHOPS_URLS="file:///opt/app-root/workshop/_workshop.yml"
-    #export CONTENT_URL_PREFIX=file:///opt/app-root/workshop
     exec /opt/workshop/bin/start-workshopper.sh
 fi
 
 if [ -f /opt/app-root/src/workshop/_workshop.yml ]; then
     export WORKSHOPS_URLS="file:///opt/app-root/src/workshop/_workshop.yml"
-    #export CONTENT_URL_PREFIX=file:///opt/app-root/src/workshop
     exec /opt/workshop/bin/start-workshopper.sh
 fi
 
