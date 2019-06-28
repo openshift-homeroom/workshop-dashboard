26643256b5d398669e5cec874ee03f757ecfd40b
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-06-28 14:29:33 +1000

    Fix AsciiDoc execute/copy.

---
+++ dashboard/renderer/static/js/workshop-asciidoc.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+    $.each([$('.execute .content'), $('.execute-1 .content')], function() {
+        if (window.location !== window.parent.location) {
+            $(this).find('.highlight').prepend('<span class="execute-glyph fas fa-play-circle" aria-hidden="true"></span>');
+            this.parent().click(function(event) {
+                if (event.shiftKey) {
+                    $(this).find('.execute-glyph').removeClass('text-danger');
+                    $(this).find('.execute-glyph').addClass('text-success');
+                    handle_copy(event);
+                }
+                else {
+                    $(this).find('.execute-glyph').removeClass('text-success');
+                    $(this).find('.execute-glyph').addClass('text-danger');
+                    handle_execute(event, 1);
+                }
+                selectElementText(this);
+            });
+        } else {
+            this.parent().prepend('<span class="copy-glyph fas fa-paste" aria-hidden="true"></span>');
+            this.parent().click(function(event) {
+                $(this).find('.copy-glyph').addClass('text-success');
+                handle_copy(event);
+                selectElementText(this);
+            });
+        }
+    });
+
+    $.each([$('.execute-2 .content')], function() {
+        if (window.location !== window.parent.location) {
+            $(this).find('.highlight').prepend('<span class="execute-glyph fas fa-play-circle" aria-hidden="true"></span>');
+            this.parent().click(function(event) {
+                if (event.shiftKey) {
+                    $(this).find('.execute-glyph').removeClass('text-danger');
+                    $(this).find('.execute-glyph').addClass('text-success');
+                    handle_copy(event);
+                }
+                else {
+                    $(this).find('.execute-glyph').removeClass('text-success');
+                    $(this).find('.execute-glyph').addClass('text-danger');
+                    handle_execute(event, 2);
+                }
+                selectElementText(this);
+            });
+        } else {
+            this.parent().prepend('<span class="copy-glyph fas fa-paste" aria-hidden="true"></span>');
+            this.parent().click(function(event) {
+                $(this).find('.copy-glyph').addClass('text-success');
+                handle_copy(event);
+                selectElementText(this);
+            });
+        }
+    });
+
+    $.each([$('.copypaste .content'), $('.copy .content')], function() {
+        $(this).find('.highlight').prepend('<span class="copy-glyph fas fa-paste" aria-hidden="true"></span>');
+        this.parent().click(function(event) {
+            $(this).find('.copy-glyph').addClass('text-success');
+            handle_copy(event);
+            selectElementText(this);
+        });
+    });
+});
--- dashboard/renderer/static/js/workshop.js
+++ dashboard/renderer/static/js/workshop.js
@@ -1,12 +1,10 @@
 function handle_execute(event, terminal) {
-    // var element = event.target.parentElement;
     var element = event.target;
     var value = element.innerText.trim();
     parent.send_to_terminal(value, terminal);
 }
 
 function handle_copy(event) {
-    // var element = event.target.parentElement;
     var element = event.target;
     var value = element.innerText.trim();
     var input = document.createElement('input');
@@ -47,70 +45,8 @@ function selectElementText(el, win) {
         range.select();
     }
 }
-
 $(document).ready(function() {
-    $.each([$('code.language-execute'), $('code.language-execute-1')], function() {
-        if (window.location !== window.parent.location) {
-            this.parent().prepend('<span class="execute-glyph fas fa-play-circle" aria-hidden="true"></span>');
-            this.parent().click(function(event) {
-                if (event.shiftKey) {
-                    $(this).find('.execute-glyph').removeClass('text-danger');
-                    $(this).find('.execute-glyph').addClass('text-success');
-                    handle_copy(event);
-                }
-                else {
-                    $(this).find('.execute-glyph').removeClass('text-success');
-                    $(this).find('.execute-glyph').addClass('text-danger');
-                    handle_execute(event, 1);
-                }
-                selectElementText(this);
-            });
-        } else {
-            this.parent().prepend('<span class="copy-glyph fas fa-paste" aria-hidden="true"></span>');
-            this.parent().click(function(event) {
-                $(this).find('.copy-glyph').addClass('text-success');
-                handle_copy(event);
-                selectElementText(this);
-            });
-        }
-    });
-
-    $.each([$('code.language-execute-2')], function() {
-        if (window.location !== window.parent.location) {
-            this.parent().prepend('<span class="execute-glyph fas fa-play-circle" aria-hidden="true"></span>');
-            this.parent().click(function(event) {
-                if (event.shiftKey) {
-                    $(this).find('.execute-glyph').removeClass('text-danger');
-                    $(this).find('.execute-glyph').addClass('text-success');
-                    handle_copy(event);
-                }
-                else {
-                    $(this).find('.execute-glyph').removeClass('text-success');
-                    $(this).find('.execute-glyph').addClass('text-danger');
-                    handle_execute(event, 2);
-                }
-                selectElementText(this);
-            });
-        } else {
-            this.parent().prepend('<span class="copy-glyph fas fa-paste" aria-hidden="true"></span>');
-            this.parent().click(function(event) {
-                $(this).find('.copy-glyph').addClass('text-success');
-                handle_copy(event);
-                selectElementText(this);
-            });
-        }
-    });
-
-    $.each([$('code.language-copy'), $('code.language-copypaste')], function() {
-        this.parent().prepend('<span class="copy-glyph fas fa-paste" aria-hidden="true"></span>');
-        this.parent().click(function(event) {
-            $(this).find('.copy-glyph').addClass('text-success');
-            handle_copy(event);
-            selectElementText(this);
-        });
-    });
-
-    $('section.content a').each(function() {
+    $('section.page-content a').each(function() {
         function normalize(path){
             path = Array.prototype.join.apply(arguments,['/'])
             var sPath;
