a7b0faaf16238ce5bdfb3c1f616f7a1f35025949
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-06-28 12:22:15 +1000

    Fix indenting.

--- dashboard/renderer/static/js/workshop.js
+++ dashboard/renderer/static/js/workshop.js
@@ -36,15 +36,15 @@ function selectElementText(el, win) {
     win = win || window;
     var doc = win.document, sel, range;
     if (win.getSelection && doc.createRange) {
-	sel = win.getSelection();
-	range = doc.createRange();
-	range.selectNodeContents(el);
-	sel.removeAllRanges();
-	sel.addRange(range);
+        sel = win.getSelection();
+        range = doc.createRange();
+        range.selectNodeContents(el);
+        sel.removeAllRanges();
+        sel.addRange(range);
     } else if (doc.body.createTextRange) {
-	range = doc.body.createTextRange();
-	range.moveToElementText(el);
-	range.select();
+        range = doc.body.createTextRange();
+        range.moveToElementText(el);
+        range.select();
     }
 }
 
