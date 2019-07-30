7527dc179384a32b9e68f26018cb1beb5dfde1c2
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-07-30 17:41:42 +1000

    Check for flags enabling components.

--- dashboard/gateway/routes/slides.js
+++ dashboard/gateway/routes/slides.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var fs = require('fs');
 
+var enable_slides = process.env.ENABLE_SLIDES;
+
 var workshop_dir = process.env.WORKSHOP_DIR || '/opt/app-root/src/workshop';
 
 var slides_dir = process.env.SLIDES_DIR;
 
 module.exports = function(app, prefix) {
     var router = express.Router();
+
+    if (enable_slides != 'true') {
+        return router;
+    }
 
     if (slides_dir) {
         if (!fs.existsSync(slides_dir + '/index.html')) {
