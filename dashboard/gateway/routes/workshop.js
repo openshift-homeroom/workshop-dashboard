eef114b51c8abd578fbbb872055c3832b7300630
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-08-20 13:13:04 +1000

    Drop Workshopper and Raneto.

--- dashboard/gateway/routes/workshop.js
+++ dashboard/gateway/routes/workshop.js
@@ -4,95 +4,30 @@ var axios = require('axios');
 var axios_retry = require('axios-retry');
 var fs = require('fs');
 
-default_renderer = process.env.DEFAULT_RENDERER;
-workshopper_urls = process.env.WORKSHOPPER_URLS || process.env.WORKSHOPS_URLS;
-
 module.exports = function(app, prefix) {
     var router = express.Router();
 
-    if (workshopper_urls || fs.existsSync('/opt/app-root/workshop/_workshop.yml') ||
-            fs.existsSync('/opt/app-root/src/workshop/_workshop.yml')) {
+    router.get('/.redirect-when-workshop-is-ready', function (req, res) {
+        var client = axios.create({ baseURL: 'http://127.0.0.1:10082' });
 
-        // Workshopper.
+        var options = {
+            retries: 3,
+            retryDelay: (retryCount) => {
+                return retryCount * 500;
+            }
+        };
 
-        router.get('/.redirect-when-workshop-is-ready', function (req, res) {
-            var client = axios.create({ baseURL: 'http://127.0.0.1:10082' });
+        axios_retry(client, options);
 
-            var options = {
-                retries: 3,
-                retryDelay: (retryCount) => {
-                    return retryCount * 500;
-                }
-            };
+        client.get(req.baseUrl + '/')
+            .then(result => {
+                res.redirect(req.baseUrl + '/');
+            });
+    })
 
-            axios_retry(client, options);
-
-            client.get(req.baseUrl + '/')
-                .then(result => {
-                    res.redirect(req.baseUrl + '/');
-                });
-        })
-
-        router.use(proxy(prefix, {
-            target: 'http://127.0.0.1:10082',
-            ws: true
-        }));
-    }
-    else {
-        if (default_renderer == 'raneto') {
-            // Raneto.
-
-            router.get('/.redirect-when-workshop-is-ready', function (req, res) {
-                var client = axios.create({ baseURL: 'http://127.0.0.1:10082' });
-
-                var options = {
-                    retries: 3,
-                    retryDelay: (retryCount) => {
-                        return retryCount * 500;
-                    }
-                };
-
-                axios_retry(client, options);
-
-                client.get('/')
-                    .then(result => {
-                        res.redirect(req.baseUrl + '/');
-                    });
-            })
-
-            router.use(proxy(prefix, {
-                target: 'http://127.0.0.1:10082',
-                pathRewrite: {
-                    ['^' + prefix]: ''
-                },
-            }));
-        }
-        else {
-            // Internal.
-
-            router.get('/.redirect-when-workshop-is-ready', function (req, res) {
-                var client = axios.create({ baseURL: 'http://127.0.0.1:10082' });
-
-                var options = {
-                    retries: 3,
-                    retryDelay: (retryCount) => {
-                        return retryCount * 500;
-                    }
-                };
-
-                axios_retry(client, options);
-
-                client.get(req.baseUrl + '/')
-                    .then(result => {
-                        res.redirect(req.baseUrl + '/');
-                    });
-            })
-
-            router.use(proxy(prefix, {
-                target: 'http://127.0.0.1:10082',
-            }));
-        }
-    }
+    router.use(proxy(prefix, {
+        target: 'http://127.0.0.1:10082',
+    }));
 
     return router;
 }
