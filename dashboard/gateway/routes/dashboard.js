338ada43d82428019f1d2aad6d7c965ff21f4323
Johnathan Kupferer <jkupfere@redhat.com>
2020-03-17 20:09:33 -0500

    Set terminal_tab disabled based on ENABLE_TERMINAL value

--- dashboard/gateway/routes/dashboard.js
+++ dashboard/gateway/routes/dashboard.js
@@ -3,6 +3,7 @@ var path = require('path');
 var fs = require('fs');
 
 var enable_dashboard = process.env.ENABLE_DASHBOARD;
+var enable_terminal = process.env.ENABLE_TERMINAL || 'true';
 
 module.exports = function(app, prefix) {
     var router = express();
@@ -13,7 +14,11 @@ module.exports = function(app, prefix) {
 
     router.locals.project_namespace = process.env.PROJECT_NAMESPACE;
 
-    router.locals.terminal_tab = process.env.TERMINAL_TAB;
+    if( enable_terminal == 'true' ) {
+        router.locals.terminal_tab = process.env.TERMINAL_TAB;
+    } else {
+        router.locals.terminal_tab = 'disabled'
+    }
 
     router.locals.console_url = process.env.CONSOLE_URL;
     router.locals.restart_url = process.env.RESTART_URL;
